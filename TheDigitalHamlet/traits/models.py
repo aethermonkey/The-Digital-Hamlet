b206a7c3d87e9b435b2546d92d07ec8af0f3835b
nengisuls <nengisuls@gmail.com>
2023-10-15 10:45:21 +1100

    Add validation constraints for the value field in the Trait model.

--- TheDigitalHamlet/traits/models.py
+++ TheDigitalHamlet/traits/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator as Min, MaxValueValidator as Max
+from django.db.models import CheckConstraint, Q
+
 
 # Create your models here.
+class Trait(models.Model):
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=200)
+    value = models.FloatField(validators=[Min(-1.0), Max(1.0)])
+
+    class Meta:
+        unique_together = ('agent', 'name')
+        constraints = (
+            # for checking range in the DB
+            CheckConstraint(
+                check=Q(value__gte=-1.0) & Q(value__lte=1.0),
+                name='value_range'),
+            )
