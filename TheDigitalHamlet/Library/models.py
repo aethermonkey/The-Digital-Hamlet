b8a79a8167f75771aa9c1955fa07f6d718af53d7
Ben C <nengisuls@gmail.com>
2023-10-15 15:19:39 +1100

    aider: Added /home/cplan/dev/The-Digital-Hamlet/plan/cheat_sheets/cheat_sheets.md

--- TheDigitalHamlet/Library/models.py
+++ TheDigitalHamlet/Library/models.py
@@ -1,25 +1,49 @@
 from TheDigitalHamlet.TheDigitalHamlet.base_models import BaseAgent
-from django.utils import timezone
+from TheDigitalHamlet.Library.conversation_models import Conversation
 
 class LibraryAgent(BaseAgent):
     def __init__(self, name, age, location, traits):
         super().__init__(name, age, location, traits)
 
     def daily_conversations_summary(self, query):
-        # Get the current date
-        current_date = timezone.now().date()
-
-        # Filter conversations for the current day
-        conversations = Conversation.objects.filter(created_at__date=current_date, message__icontains=query)
+        # Search conversations based on the provided query
+        conversations = Conversation.objects.filter()
 
         # Create a summary of daily conversations
         summary = ""
         for conversation in conversations:
             summary += f"Conversation between {', '.join(str(agent) for agent in conversation.agents)}\n"
+            summary += f"Created: {conversation.created_at}"
             summary += f"Message: {conversation.message}\n"
             summary += "------------------------\n"
+
+        completion = self
 
         # Store the summary as knowledge
         self.store_knowledge(summary)
 
-    # ... rest of the code ...
+    def store_knowledge(self, knowledge):
+        # Store the knowledge in a structured format
+        knowledge_entry = Knowledge.objects.create(title="Daily Conversations Summary", content=knowledge, medium="Text", classification="Summary")
+        knowledge_entry.save()
+
+    def search_knowledge(self, query):
+        # Search the knowledge system for information based on the query
+        knowledge_entries = Knowledge.objects.filter(content__icontains=query)
+        return knowledge_entries
+
+    def advise_municipal_agents(self):
+        # Advise the municipal agents on high-level descriptions of how the Library can achieve a knowledge system fit for Artificial General Intelligence
+        pass
+
+    def liaise_with_agents(self, agents):
+        # Liaise with other agents to provide information from the knowledge system
+        pass
+
+    def check_authorisation(self, agent):
+        # Check the authorisation level of an agent to control access to information
+        pass
+
+class LibraryManager(BaseAgent):
+    def __init__(self, name, age, location, traits):
+        super().__init__(name, age, location, traits)
