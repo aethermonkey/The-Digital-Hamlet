3b974a989ce578dfd784e22d8784498229267cd4
Ben C <nengisuls@gmail.com>
2023-10-15 13:45:39 +1100

    aider: Change the agents field in the Conversation model to a JSONField.
    # Aider chat conversation:
    
    USER: why is library agent and sovereign agent passed to the many to many fields
    
    ASSISTANT: No changes are needed.

--- TheDigitalHamlet/Library/conversation_models.py
+++ TheDigitalHamlet/Library/conversation_models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 class Conversation(models.Model):
-    agents = models.ManyToManyField('LibraryAgent')
+    agents = JSONField()
     users = models.ManyToManyField('SovereignUser')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation between {', '.join(str(agent) for agent in self.agents.all())}"
+        return f"Conversation between {', '.join(str(agent) for agent in self.agents)}"
