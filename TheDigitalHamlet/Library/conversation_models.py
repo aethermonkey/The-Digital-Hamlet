d3eed120ca8e60dc3e0e0a7a8ff9bd141951365e
Ben C <nengisuls@gmail.com>
2023-10-15 13:42:27 +1100

    aider: Refactor conversation model to use a list or dict for agents and users.
    # Aider chat conversation:
    
    USER: in conversation models I would prefer agents be a list or dict as there will often be more than two agents involved, and possibly multiple real human users too.
    
    ASSISTANT: No changes are needed.

--- TheDigitalHamlet/Library/conversation_models.py
+++ TheDigitalHamlet/Library/conversation_models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Conversation(models.Model):
-    agent1 = models.ForeignKey('LibraryAgent', on_delete=models.CASCADE)
-    agent2 = models.ForeignKey('LibraryAgent', on_delete=models.CASCADE)
-    user = models.ForeignKey('SovereignUser', on_delete=models.CASCADE)
+    agents = models.ManyToManyField('LibraryAgent')
+    users = models.ManyToManyField('SovereignUser')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation between {self.agent1} and {self.agent2}"
+        return f"Conversation between {', '.join(str(agent) for agent in self.agents.all())}"
