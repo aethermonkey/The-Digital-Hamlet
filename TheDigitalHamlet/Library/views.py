9b21a815d0f77c9902c5abfe371461c1d79fbac1
nengisuls <nengisuls@gmail.com>
2023-10-19 16:57:05 +1100

    Changed the template path for the library_agent_chat view.

--- TheDigitalHamlet/Library/views.py
+++ TheDigitalHamlet/Library/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 def library_agent_chat(request):
-    return render(request, 'Library/library_agent_chat.html')
+    return render(request, 'Library/templates/library_agent_chat.html')
