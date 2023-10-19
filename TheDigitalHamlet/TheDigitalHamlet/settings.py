09e2dd0828fdef936d4ea92efdc00075700b1307
nengisuls <nengisuls@gmail.com>
2023-10-19 17:00:14 +1100

    Added the directory "Library/templates" to the DIRS list in settings.py.

--- TheDigitalHamlet/TheDigitalHamlet/settings.py
+++ TheDigitalHamlet/TheDigitalHamlet/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "TheDigitalHamlet.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "Library/templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
