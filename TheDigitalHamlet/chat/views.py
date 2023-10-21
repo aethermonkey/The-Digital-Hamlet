c4c3ceb680428900012be6202820988100e84230
nengisuls <nengisuls@gmail.com>
2023-10-21 20:05:02 +1100

    chat room looking better

--- TheDigitalHamlet/chat/views.py
+++ TheDigitalHamlet/chat/views.py
@@ -6,6 +6,7 @@ def lobby(request):
     return render(request, "chat/lobby.html")
 
 def room(request, room_name):
+    print(room_name)
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
