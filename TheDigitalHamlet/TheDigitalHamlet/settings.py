6b6626a765983f11e53e0adb85ffc1f116e49d3c
nengisuls <nengisuls@gmail.com>
2023-10-23 10:57:46 +1100

    chat working with redis

--- TheDigitalHamlet/TheDigitalHamlet/settings.py
+++ TheDigitalHamlet/TheDigitalHamlet/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    "chat",
     "TheDigitalHamlet",
     "Library",
     "Towncenter",
     "Civic",
     "SovereignInstitute",
-    "chat",
-    "daphne",
+
     "channels",
 
     "django.contrib.admin",
@@ -133,8 +134,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
